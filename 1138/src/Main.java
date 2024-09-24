a7c5d82883d6b0c974f0e6408e6efb12a1e682e7
Argen <argennazaraliev4@gmail.com>
2024-09-24 21:27:36 +0600

    update

---
+++ 1138/src/Main.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        while (true) {
+            int A = scanner.nextInt();
+            int B = scanner.nextInt();
+            if (A == 0 && B == 0) break;
+
+            int[] digitCount = new int[10];
+            for (int i = A; i <= B; i++) {
+                for (char digit : Integer.toString(i).toCharArray()) {
+                    digitCount[digit - '0']++;
+                }
+            }
+
+            for (int count : digitCount) {
+                System.out.print(count + " ");
+            }
+            System.out.println();
+        }
+
+        scanner.close();
+    }
+}

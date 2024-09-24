511b56671ec769c5dc9ef6d2086ef27eff6e92d1
Argen <argennazaraliev4@gmail.com>
2024-09-24 21:25:23 +0600

    update

--- 1170/src/Main.java
+++ 1170/src/Main.java
@@ -5,39 +5,16 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
 
         int N = scanner.nextInt();
-        scanner.nextLine();
+        for (int i = 0; i < N; i++) {
+            double X = scanner.nextDouble();
+            int days = 0;
 
-        for (int i = 1; i <= N; i++) {
-            String line = scanner.nextLine();
-            String[] parts = line.split(" ");
-            String numberStr = parts[0];
-            String format = parts[1];
-
-            int decimalValue = 0;
-
-            if (format.equals("dec")) {
-                decimalValue = Integer.parseInt(numberStr);
-            } else if (format.equals("bin")) {
-                decimalValue = Integer.parseInt(numberStr, 2);
-            } else if (format.equals("hex")) {
-                decimalValue = Integer.parseInt(numberStr, 16);
+            while (X > 1.0) {
+                X /= 2;
+                days++;
             }
 
-            String decimalOutput = decimalValue + " dec";
-            String hexOutput = Integer.toHexString(decimalValue) + " hex";
-            String binaryOutput = Integer.toBinaryString(decimalValue) + " bin";
-
-            System.out.println("Case " + i + ":");
-            if (!format.equals("dec")) {
-                System.out.println(decimalOutput);
-            }
-            if (!format.equals("hex")) {
-                System.out.println(hexOutput);
-            }
-            if (!format.equals("bin")) {
-                System.out.println(binaryOutput);
-            }
-            System.out.println();
+            System.out.println(days + " dias");
         }
 
         scanner.close();

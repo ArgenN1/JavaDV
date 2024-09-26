36b6b41f6caef773bfe8c6805cd8f69c6627a70d
Argen <argennazaraliev4@gmail.com>
2024-09-26 19:35:46 +0600

    update

---
+++ BeeCrowd/src/BC1170.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class BC1170 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            double X = scanner.nextDouble();
+            int days = 0;
+
+            while (X > 1) {
+                X /= 2;
+                days++;
+            }
+
+            System.out.println(days + " dias");
+        }
+
+        scanner.close();
+    }
+}
